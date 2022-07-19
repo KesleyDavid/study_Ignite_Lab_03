d261a8950422553e23cbd953d97599705c0d162e
Kesley DEV <kesleygti@gmail.com>
2022-07-19 20:01:39 -0300

    feat: create statusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { NativeBaseProvider } from 'native-base'
+import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 import { SignIn } from './src/screens/SignIn'
@@ -11,6 +11,11 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   )
