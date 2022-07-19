584b07b10daf42cec67894b2355790ea7330201c
Kesley DEV <kesleygti@gmail.com>
2022-07-19 19:40:00 -0300

    feat: integrate native base

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,10 @@
+import { NativeBaseProvider, Box } from "native-base";
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   return (
-    <SignIn />
+    <NativeBaseProvider>
+      <SignIn />
+    </NativeBaseProvider>
   );
 }
