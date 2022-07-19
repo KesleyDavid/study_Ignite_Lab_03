587e98451aed21c3895ff80ba54b43af3ce88f17
Kesley DEV <kesleygti@gmail.com>
2022-07-19 19:59:24 -0300

    feat: install font

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,17 @@
 import { NativeBaseProvider } from 'native-base'
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+
 import { SignIn } from './src/screens/SignIn'
+import { Loading } from './src/components/Loading';
 
 import { THEME} from './src/styles/theme'
 
 export default function App() {
+  let [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   )
 }
---
+++ mobile/_dev/info.md
@@ -0,0 +1,10 @@
+npm install --global expo-cli
+expo login
+expo init rockethelp
+cd mobile
+expo start
+yarn add native-base
+expo install react-native-svg
+expo install react-native-safe-area-context
+expo start
+expo install expo-font @expo-google-fonts/roboto
